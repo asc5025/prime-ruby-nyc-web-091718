df5287f3ac153b7f0fa3a9d1307a85ac5dec1be2
Albert Chun <albert.s.chun@gmail.com>
2018-09-07 17:53:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+#def prime?(number)
+#  if number <= 1 
+#    return false
+#    elsif number == 2
+#     return true
+#  else
+#    num_array = (2..number - 1).to_a
+#    num_array.each do |num|
+#     if number % num != 0 
+#     return false
+#     end
+#   return true
+# end
+# end
+#end
+
+def prime?(number)
+  if number <= 1
+    return false
+    elsif number == 2
+    return true
+    else 
+        (2..number/2).none? { |num| number % num == 0}
+    end
+end
